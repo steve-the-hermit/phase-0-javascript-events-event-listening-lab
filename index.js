caca1b30d06e2bf5819f4b03d9f62532ce37128c
steve-the-hermit <stevemacharia309@gmail.com>
2023-06-22 23:47:19 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input'); // Get the input element
+  
+    input.addEventListener('click', function() {
+      // Event listener callback function
+      // Add your desired functionality here
+      console.log('Input clicked!');
+    });
+  }
+  
